4cd1ef77ff6f06367e3c04b392dbb2fa72e475a0
blinkysc <37940565+blinkysc@users.noreply.github.com>
2025-11-17 20:40:11 -0600

    Update glyph spell scripts and item names
    Removed old spell script bindings and updated glyph entries.

--- data/sql/db-world/classic_glyphs.sql
+++ data/sql/db-world/classic_glyphs.sql
@@ -1,19 +1,5 @@
--- Classic Glyphs Module
--- Implements WotLK Classic 3.4.3 glyphs by repurposing existing 3.3.5a glyphs
-
 -- Bind spell scripts to the abilities
 DELETE FROM `spell_script_names` WHERE `ScriptName` IN ('spell_dru_faerie_fire_feral_glyph', 'spell_pal_hand_of_reckoning_glyph');
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (16857, 'spell_dru_faerie_fire_feral_glyph'),  -- Faerie Fire (Feral)
 (62124, 'spell_pal_hand_of_reckoning_glyph'); -- Hand of Reckoning
-
--- Update item names for server-side display (client will still show original names)
-UPDATE `item_template` SET
-    `name` = 'Glyph of Omen of Clarity',
-    `description` = 'Your Omen of Clarity talent has a 100% chance to be triggered by successfully casting Faerie Fire (Feral). Does not trigger on players or player-controlled pets.'
-WHERE `entry` = 40899;
-
-UPDATE `item_template` SET
-    `name` = 'Glyph of Reckoning',
-    `description` = 'Your Hand of Reckoning spell no longer taunts the target and can deal damage to untauntable targets.'
-WHERE `entry` = 45743;
